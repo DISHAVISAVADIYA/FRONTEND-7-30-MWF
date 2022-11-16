c90e300fe54dd8bb8aa875ee9d0da15fc93040c5
DISHAVISAVADIYA <109840398+DISHAVISAVADIYA@users.noreply.github.com>
2022-11-16 11:17:34 +0530

    Add files via upload

---
+++ javascript introduction/javascript output method/external js.js
@@ -0,0 +1,8 @@
+// function test(){
+//     var a=document.getElementById("qty").Value;
+//     var b=document.getElementById("price").value;
+//     var c=a*b;
+//     document.getElementById("total").innerHTML=c;
+// }
+
+document.write("helo")
