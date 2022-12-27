d3ace4e32dff113b762079d44ed1c340a7e32fac
DISHAVISAVADIYA <109840398+DISHAVISAVADIYA@users.noreply.github.com>
2022-12-27 14:32:15 +0530

    Add files via upload

---
+++ task/list-app/src/List.js
@@ -0,0 +1,24 @@
+import React from "react";
+function List(){
+return(
+    <>
+    <h1>welcome to the page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis vitae harum sed sint voluptatibus, assumenda magnam commodi dolorum, alias voluptatum minus quas eius quaerat iusto et in, laboriosam fugiat? Nesciunt quidem harum ipsam quo aliquid corrupti inventore aliquam alias tenetur debitis, porro, esse aperiam vel, est itaque maiores voluptates eius?
+    </p>
+    <ol>
+        <li>sunday</li>
+        <li>monday</li>
+        <li>tuesday</li>
+        <li>wednesday</li>
+        <li>thusday</li>
+        <li>friday</li>
+        <li>satureday</li>
+
+
+    </ol>
+    </>
+    
+    
+);
+}
+export default List;
---
+++ task/list-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import List from './List';
+
+const abc = ReactDOM.createRoot(document.getElementById('demo'));
+abc.render(
+    <React.StrictMode>
+<List />
+     </React.StrictMode>
+)
+;
